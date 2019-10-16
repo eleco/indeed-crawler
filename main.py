c607f4dd420d83490dfd86c7c5033a120c1d0473
eleco <eric.leconte@gmail.com>
2019-10-16 16:23:50 +0100

    use df column names instead of indexes

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import requests
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 import time
+from collections import defaultdict
 
-max_pages=2
+max_pages=1
 host="http://indeed.fr"
 max_pages=2
 cities=['Rennes,+France','Lyon,+France']
@@ -28,13 +29,25 @@ with requests.Session() as s:
             locations = [item.text.strip() for item in soup.select('span.location')]
             dates = [item.text.strip() for item in soup.select('span.date')]
             hrefs = [ host+url['href'] for url in urls]
-            results.append(list(zip(titles, companies, locations, dates, hrefs)))
+
+            list_of_tuples  = list(zip(titles, companies, locations, dates, hrefs))
+            results = results + list_of_tuples
 
 
+df = pd.DataFrame(results, columns = ['title','company','location','date','href'])
 
-df = pd.DataFrame([item for sublist in results for item in sublist])
-df['joburl'] = '<a href="' + df.iloc[:,4]+ '">' + df.iloc[:,0] + '</a>'
-df.drop(df.columns[[0,4]],axis=1,inplace=True)
+
+# drop adverts which are not of today
+d = defaultdict(lambda: -1)
+d.update({'Aujourd\'hui': 0, 'Publiée à l\'instant': 0})
+df['normalized_date'] = df.iloc[:,3].map(d)
+df = df[df.normalized_date != -1]
+
+# add href to the job advert
+df['joburl'] = '<a href="' + df['href']+ '">' + df['title'] + '</a>'
+df = df.drop(columns=['href', 'date' , 'normalized_date'])
+
+# generate output
 print(df)
 df.to_html("test.html",escape=False)
 
