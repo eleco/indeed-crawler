a233e362e60f466ee39262215987447a80fdb737
eleco <eric.leconte@gmail.com>
2019-10-17 16:02:37 +0100

    prettify the output + fetch from different sites

--- main.py
+++ main.py
@@ -4,51 +4,64 @@ import pandas as pd
 import time
 from collections import defaultdict
 
-max_pages=1
-host="http://indeed.fr"
-max_pages=2
-cities=['Rennes,+France','Lyon,+France']
 results = []
-url_prefix = host + '/jobs?q=java'
-url_suffix='&sort=date&start={}'
+url_suffix='&sort=date&fromage=1&start={}'
 
+
+#search criteria
+cities ={
+    'http://indeed.fr':['Rennes,+France','Lyon,+France'],
+    'https://de.indeed.com':['München','Berlin']}
+
+max_pages=2
 
 
 with requests.Session() as s:
 
-    for city in cities:
+    for host in cities:
     
-        for page in range(max_pages):
-            time.sleep(1)
-            url=url_prefix + '&l=' + city + url_suffix
-            res = s.get(url.format(page))
-            soup = bs(res.content, 'lxml')
-            urls = [item for item in soup.select('.title > a')]
-            titles = [item.text.strip() for item in soup.select('[data-tn-element=jobTitle]')]
-            companies = [item.text.strip() for item in soup.select('.company')]
-            locations = [item.text.strip() for item in soup.select('span.location')]
-            dates = [item.text.strip() for item in soup.select('span.date')]
-            hrefs = [ host+url['href'] for url in urls]
+        for city in cities[host]:
+        
+            for page in range(max_pages):
+                time.sleep(1)
+                url= host + '/jobs?q=java' + '&l=' + city + url_suffix
+                res = s.get(url.format(page))
+                soup = bs(res.content, 'lxml')
+                urls = [item for item in soup.select('.title > a')]
+                titles = [item.text.strip() for item in soup.select('[data-tn-element=jobTitle]')]
+                companies = [item.text.strip() for item in soup.select('.company')]
+                locations = [item.text.strip() for item in soup.select('span.location')]
+                hrefs = [ host+url['href'] for url in urls]
 
-            list_of_tuples  = list(zip(titles, companies, locations, dates, hrefs))
-            results = results + list_of_tuples
-
-
-df = pd.DataFrame(results, columns = ['title','company','location','date','href'])
+                list_of_tuples  = list(zip(titles, companies, locations, hrefs))
+                results = results + list_of_tuples
 
 
-# drop adverts which are not of today
-d = defaultdict(lambda: -1)
-d.update({'Aujourd\'hui': 0, 'Publiée à l\'instant': 0})
-df['normalized_date'] = df.iloc[:,3].map(d)
-df = df[df.normalized_date != -1]
+df = pd.DataFrame(results, columns = ['title','company','location','href'])
+
 
 # add href to the job advert
 df['joburl'] = '<a href="' + df['href']+ '">' + df['title'] + '</a>'
-df = df.drop(columns=['href', 'date' , 'normalized_date'])
+df = df.drop(columns=['href'])
+print(df)
 
 # generate output
-print(df)
-df.to_html("test.html",escape=False)
+pd.set_option('colheader_justify', 'center')   # FOR TABLE <th>
+
+html_string = '''
+<html>
+  <head><title>HTML Pandas Dataframe with CSS</title></head>
+  <link rel="stylesheet" type="text/css" href="df_style.css"/>
+  <body>
+    {table}
+  </body>
+</html>.
+'''
+
+# OUTPUT AN HTML FILE
+with open('test.html', 'w') as f:
+    f.write(html_string.format(table=df.to_html(escape=False, classes='mystyle')))
+
+#df.to_html("test.html",escape=False)
 
 
